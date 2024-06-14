3016c496e1cca8a6b23408b3bfa4d1a378faebcb
minsu-zip <alstn4814@gmail.com>
2024-06-15 01:15:28 +0900

    setting: pwa 환경 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
-import withPWA from "next-pwa";
+import withPWAInit from "@ducanh2912/next-pwa"
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {}
 
-export default withPWA({ ...nextConfig, dest: "public" });
+const isProd = process.env.NODE_ENV === "production" // 운영환경에서만 pwa 적용
+const config = isProd ? withPWAInit({ dest: "public" })(nextConfig) : nextConfig
+
+export default config
