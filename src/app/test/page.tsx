edacc0d58f1f709ee26cf3bfa882242a3ce8ff64
minsu-zip <alstn4814@gmail.com>
2024-06-20 21:26:01 +0900

    chore: suspense, errorboundary 사용법 및 설명 추가

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,17 +1,17 @@
 import { Suspense } from "react"
 
-import ErrorHandlingWrapper from "@/components/ErrorHandlingWrapper"
 import Test from "@/components/Test"
 
 const TestPage = () => {
   return (
-    // 각 페이지마다 ErrorHandlingWrapper로 감싸줍니다.
-    // react-query의 QueryErrorResetBoundary를 이용하여 에러가 발생했을 때, 에러를 잡아주고 에러가 발생했을 때 보여줄 컴포넌트를 정의합니다.
-    <ErrorHandlingWrapper>
+    // 해당 페이지에서 에러가 발생했을 때는 해당 depth에 있는 error.tsx 컴포넌트를 보여줍니다.
+    // 해당 페이지에서 로딩 중일 떄는 header태그를 제외한 영역만 suspense로 인해 로딩 처리됩니다.
+    <>
+      <header>로딩이 없음</header>
       <Suspense fallback={<div>특정 범위 loading...</div>}>
         <Test />
       </Suspense>
-    </ErrorHandlingWrapper>
+    </>
   )
 }
 export default TestPage
