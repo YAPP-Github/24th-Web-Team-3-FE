20f1b5070ad2cb846adb74cdc374c1a0ff43dea5
minsu-zip <alstn4814@gmail.com>
2024-06-22 01:08:11 +0900

    feat: customFetch 정의 및 사용 예시 함수 정의

---
+++ src/app/api/index.ts
@@ -0,0 +1,30 @@
+import customFetch from "../_lib/customFetch"
+
+const myFetch = customFetch({
+  baseUrl: "",
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Bearer token",
+  },
+  interceptors: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    request: async ([url, options], fetch) => {
+      // 요청 인터셉터 로직 추가 (예: 로깅, 인증 토큰 추가)
+      //   console.log("Request Interceptor:", url, options)
+      return [url, options]
+    },
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    response: async (response, [url, options], fetch) => {
+      // 응답 인터셉터 로직 추가 (예: 에러 처리, 응답 데이터 변환)
+      //   console.log("Response Interceptor:", response, url, options, fetch)
+      return response
+    },
+  },
+})
+
+export const getJsonplaceholder = async (url?: string) => {
+  await new Promise((resolve) => setTimeout(resolve, 700))
+  return myFetch(`https://jsonplaceholder.typicode.com/todos/${url ?? ""}`, {
+    method: "GET",
+  })
+}
