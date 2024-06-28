95c365369a78034f310a0229a147acd3067d3670
minsu-zip <alstn4814@gmail.com>
2024-06-29 00:47:17 +0900

    feat: qr-code 네컷 브랜드url 판별

---
+++ src/app/qr/lib/index.ts
@@ -0,0 +1,14 @@
+export const FOUR_CUT_BRAND = {
+  PHOTOISM: "https://qr.seobuk.kr",
+  DONTLXXKUP: "https://x.dontlxxkup.kr",
+  HARUFILM: "http://haru6.mx2.co.kr",
+}
+
+// 객체의 value들을 정규표현식 패턴으로 결합
+const urlPatterns = Object.values(FOUR_CUT_BRAND)
+  .map((url) => url.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&") + ".*")
+  .join("|")
+
+// 입력된 문자열이 객체의 value에 포함되어 있는지 확인하는 함수
+export const isUrlIncluded = (input: string) =>
+  new RegExp(`^(${urlPatterns})$`).test(input)
