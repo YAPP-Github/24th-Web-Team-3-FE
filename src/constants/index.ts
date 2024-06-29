eae6a2a2c3d9d2629c0d958892bedda5f64b4bd0
minsu-zip <alstn4814@gmail.com>
2024-06-30 02:33:11 +0900

    feat: sns login flow 구현

---
+++ src/constants/index.ts
@@ -0,0 +1,1 @@
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_REDIRECT_URI}&prompt=select_account`
