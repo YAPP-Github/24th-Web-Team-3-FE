77c33ec8d7d96a3a5b89eba6e2595cf2b2a391bf
minsu-zip <alstn4814@gmail.com>
2024-07-01 23:31:24 +0900

    qr  페이지 스타일링

--- src/app/qr/page.tsx
+++ src/app/qr/page.tsx
@@ -6,7 +6,13 @@ import { IDetectedBarcode, Scanner } from "@yudiel/react-qr-scanner"
 import { isUrlIncluded } from "./lib"
 
 const style = {
-  container: { width: "100%", height: "100%" },
+  container: {
+    display: "flex !important",
+    color: "red",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  finderBorder: 100,
 }
 
 const QrPage = () => {
@@ -28,7 +34,20 @@ const QrPage = () => {
         styles={style}
         scanDelay={500}
         allowMultiple={true}
-      />
+        components={{
+          onOff: true,
+          finder: false,
+        }}>
+        <>
+          <p className="text-header-2 font-bold text-white clear">홈</p>
+          <div>
+            <button>버튼 텍스트</button>
+          </div>
+          <div className="fullscreen-qr-scanner__overlay">
+            <div className="fullscreen-qr-scanner__overlay__inner" />
+          </div>
+        </>
+      </Scanner>
     </div>
   )
 }
