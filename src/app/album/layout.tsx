6bf25c4bf411af713c515ca08a0ed709d80e4ab4
MayOwall <pixartive@gmail.com>
2024-07-03 18:14:29 +0900

    feat: /album 및 하위 경로에서 적용되는 layout 구현

---
+++ src/app/album/layout.tsx
@@ -0,0 +1,14 @@
+import { Header } from "@/components"
+
+const AlbumLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="w-full min-h-dvh flex justify-center bg-gray-500">
+      <div className="max-w-[390px] w-full bg-white">
+        <Header />
+        {children}
+      </div>
+    </div>
+  )
+}
+
+export default AlbumLayout
