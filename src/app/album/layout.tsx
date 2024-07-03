2090a9310e9357de9c6c795301a4b01dfa7c7ce6
MayOwall <pixartive@gmail.com>
2024-07-03 18:44:23 +0900

    fix: /album/create css layout 문제 해결

--- src/app/album/layout.tsx
+++ src/app/album/layout.tsx
@@ -3,7 +3,7 @@ import { Header } from "@/components"
 const AlbumLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-full min-h-dvh flex justify-center bg-gray-500">
-      <div className="max-w-[390px] w-full bg-white">
+      <div className="max-w-[390px] w-full bg-white relative">
         <Header />
         {children}
       </div>
