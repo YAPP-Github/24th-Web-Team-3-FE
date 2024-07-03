43ed543ba4c596a1f0af5cba8b253bcaba4d2dcc
MayOwall <pixartive@gmail.com>
2024-07-03 18:22:28 +0900

    feat: /album/create 페이지 UI 구현

---
+++ src/app/album/create/page.tsx
@@ -0,0 +1,14 @@
+import { AlbumEditSection } from "@/components"
+
+const AlbumCreatePage = () => {
+  return (
+    <>
+      <h1 className="p-6 text-header-2 font-bold">
+        새로운 앨범을 만들어주세요
+      </h1>
+      <AlbumEditSection />
+    </>
+  )
+}
+
+export default AlbumCreatePage
