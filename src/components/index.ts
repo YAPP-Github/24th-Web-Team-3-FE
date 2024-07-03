29e32dec560542082075e5fb303811b49f39c430
MayOwall <pixartive@gmail.com>
2024-07-03 18:21:41 +0900

    feat: AlbumEditSection 컴포넌트 구현

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,8 @@ export { Badge } from "./Badge"
 export { Button } from "./Button"
 export { ColorIcon } from "./ColorIcon"
 export { FloatingButton } from "./FloatingButton"
+export { Header } from "./Header"
 export { Icon } from "./Icon"
+
+// organisms
+export { AlbumEditSection } from "./organisms/AlbumEditSection"
---
+++ src/components/organisms/AlbumEditSection.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import { useState } from "react"
+
+import { AlbumItem, Button, ColorIcon } from "@/components"
+import type { albumType, albumValue } from "@/types"
+
+export function AlbumEditSection({
+  albumValue: albumValueInit = DEFAULT_ALBUM_VALUE,
+}: AlbumCreateSectionProps) {
+  const [value, setValue] = useState(albumValueInit)
+  const { type } = value
+
+  const handleType = (type: albumType) => {
+    const nextValue = {
+      ...value,
+      type,
+    }
+    setValue(() => nextValue)
+  }
+
+  const handleValue = (v: albumValue) => {
+    setValue(() => v)
+  }
+
+  const handleSubmit = () => {
+    // TODO : Album 생성 API 적용
+    // TODO : Album 상세 페이지로의 이동 구현
+  }
+
+  return (
+    <>
+      <div className="w-full flex justify-center pt-6">
+        <AlbumItem value={value} handleValue={handleValue} />
+      </div>
+      <div className="w-full flex flex-col items-center absolute bottom-0">
+        <AlbumTypeSelectTab type={type} handleType={handleType} />
+        <Button
+          className="w-[calc(100%-3rem)] mt-3 mb-11"
+          onClick={handleSubmit}>
+          다음으로
+        </Button>
+      </div>
+    </>
+  )
+}
+
+function AlbumTypeSelectTab({ type, handleType }: AlbumTypeSelectTabProps) {
+  const colors = ["red", "yellow", "green", "blue", "purple", "pink"] as const
+
+  return (
+    <div className="w-full h-20 py-4 px-8 flex justify-between items-center">
+      {colors.map((color) => (
+        <ColorIcon
+          key={color}
+          iconColor={color}
+          size={color === type ? "large" : "medium"}
+          className={color !== type ? "opacity-30" : ""}
+          onClick={() => handleType(color)}
+        />
+      ))}
+    </div>
+  )
+}
+
+interface AlbumCreateSectionProps extends React.HTMLAttributes<HTMLDivElement> {
+  albumValue?: albumValue
+}
+
+interface AlbumTypeSelectTabProps {
+  type: albumType
+  handleType: (type: albumType) => void
+}
+
+const DEFAULT_ALBUM_VALUE: albumValue = {
+  name: "",
+  type: "red",
+  photoCount: 0,
+  isNew: false,
+  isSelected: false,
+  isEditable: true,
+}
