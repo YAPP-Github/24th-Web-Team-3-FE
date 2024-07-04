0402f8cfa2987df26df30b367a82db56b52e9215
minsu-zip <alstn4814@gmail.com>
2024-07-04 22:38:19 +0900

    feat: tailwindCss 스타일링

--- src/app/qr/page.tsx
+++ src/app/qr/page.tsx
@@ -1,22 +1,26 @@
 "use client"
-import "./page.css" // 스타일 정의를 위한 CSS 파일을 포함합니다.
 
 import { IDetectedBarcode, Scanner } from "@yudiel/react-qr-scanner"
 
+import { postQrCode } from "../api"
 import { isUrlIncluded } from "./lib"
 
 const style = {
   container: {
-    display: "flex !important",
+    display: "flex",
     color: "red",
     justifyContent: "center",
     alignItems: "center",
   },
-  finderBorder: 100,
+  video: {
+    width: "100%",
+    height: "100%",
+    "object-fit": "cover",
+  },
 }
 
 const QrPage = () => {
-  const onScan = (result: IDetectedBarcode[]) => {
+  const onScan = async (result: IDetectedBarcode[]) => {
     const { rawValue } = result[0]
 
     if (!isUrlIncluded(rawValue)) {
@@ -24,14 +28,14 @@ const QrPage = () => {
       return
     }
 
-    alert(rawValue)
+    await postQrCode(rawValue)
   }
 
   return (
-    <div className="fullscreen-qr-scanner">
+    <div className="h-[100vh] overflow-hidden">
       <Scanner
         onScan={onScan}
-        styles={style}
+        styles={{ ...style }}
         scanDelay={500}
         allowMultiple={true}
         components={{
@@ -39,12 +43,23 @@ const QrPage = () => {
           finder: false,
         }}>
         <>
-          <p className="text-header-2 font-bold text-white clear">홈</p>
-          <div>
-            <button>버튼 텍스트</button>
+          <p className="relative z-[1] px-4 py-3.5 text-header-2 font-bold text-white">
+            홈
+          </p>
+          <div className="relative z-[1] flex flex-col items-center justify-center p-6">
+            <p className="whitespace-pre text-center text-title-1 text-border text-white">
+              {`QR코드를 스캔해\n인생네컷을 저장해요`}
+            </p>
           </div>
-          <div className="fullscreen-qr-scanner__overlay">
-            <div className="fullscreen-qr-scanner__overlay__inner" />
+          <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+            <div className="bor h-[345px] w-[345px] rounded-3xl border-4 border-solid border-green-600 shadow-[0_0_0_100vh_rgba(24,28,35,0.8)]" />
+            <p className="p-6 text-center text-body-2 font-medium text-white">
+              지원하지 않는 브랜드라면 웹사이트를 열어드려요.
+            </p>
+          </div>
+
+          <div className="absolute bottom-0 left-0 z-[1] h-[106px] w-full rounded-t-2xl bg-gray-900 px-[54px] pb-[32px] pt-[20px]">
+            {/* 아이콘 컴포넌투 추가 */}
           </div>
         </>
       </Scanner>
