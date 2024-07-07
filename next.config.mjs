13f65907b234a637dcf9a29b36df84390696dba8
minsu-zip <alstn4814@gmail.com>
2024-07-07 18:30:10 +0900

    chore: redirect config 설정

--- next.config.mjs
+++ next.config.mjs
@@ -23,8 +23,16 @@ const nextConfig = {
         hostname: "**",
       },
     ],
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true,
+      },
+    ]
   },
-}
+}}
 
 const isProd = process.env.NODE_ENV === "production" // 운영환경에서만 pwa 적용
 const config = isProd ? withPWAInit({ dest: "public" })(nextConfig) : nextConfig
