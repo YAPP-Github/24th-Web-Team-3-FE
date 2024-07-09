23922cf812454f54be5ff4cf54ef5e2439559822
MayOwall <pixartive@gmail.com>
2024-07-09 15:20:12 +0900

    fix: next.config에서 image key 오류 해결

--- next.config.mjs
+++ next.config.mjs
@@ -23,6 +23,7 @@ const nextConfig = {
         hostname: "**",
       },
     ],
+  },
   async redirects() {
     return [
       {
@@ -32,7 +33,7 @@ const nextConfig = {
       },
     ]
   },
-}}
+}
 
 const isProd = process.env.NODE_ENV === "production" // 운영환경에서만 pwa 적용
 const config = isProd ? withPWAInit({ dest: "public" })(nextConfig) : nextConfig
