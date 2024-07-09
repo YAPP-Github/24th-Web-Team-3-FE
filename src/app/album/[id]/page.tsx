0632a1cd17f5c69e0da69657313e2e793c86f846
MayOwall <pixartive@gmail.com>
2024-07-09 14:42:38 +0900

    modfiy: Layout에서 Header 컴포넌트 제거

--- src/app/album/[id]/page.tsx
+++ src/app/album/[id]/page.tsx
@@ -1,12 +1,16 @@
-import { AlbumPhotos } from "@/components"
+import { AlbumPhotos } from "./_component/AlbumPhotos"
+import { Header } from "./_component/Header"
 
 const AlbumDetailPage = ({ params }: { params: { id: string } }) => {
   const { id } = params
 
   return (
-    <section className="w-full p-4 px-6 flex flex-wrap border">
-      <AlbumPhotos albumId={id} />
-    </section>
+    <>
+      <Header albumId={id} />
+      <section className="w-full p-4 px-6 flex flex-wrap">
+        <AlbumPhotos albumId={id} />
+      </section>
+    </>
   )
 }
 
--- src/app/album/layout.tsx
+++ src/app/album/layout.tsx
@@ -1,12 +1,7 @@
-import { Header } from "@/components"
-
 const AlbumLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="w-full min-h-dvh flex justify-center bg-gray-500">
-      <div className="max-w-[390px] w-full bg-white relative">
-        <Header />
-        {children}
-      </div>
+      <div className="max-w-[390px] w-full bg-white relative">{children}</div>
     </div>
   )
 }
