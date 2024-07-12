4cebc0da7148f551ff90c04803cacf71692877bd
minsu-zip <alstn4814@gmail.com>
2024-07-13 00:11:52 +0900

    feat: 카카오톡 프로필 조회 api

--- src/app/api/login.ts
+++ src/app/api/login.ts
@@ -5,10 +5,23 @@ interface AuthLoginResponse {
   refreshToken: string
 }
 
+export interface GetMyProfileResponse {
+  memberId: string
+  name: string
+}
+
 export const authLogin = async (code: string): Promise<AuthLoginResponse> => {
   const data = await myFetch("user/v1/auth/login/kakao", {
     method: "POST",
     body: JSON.stringify({ code }),
+  })
+
+  return data
+}
+
+export const getMyProfile = async (): Promise<GetMyProfileResponse> => {
+  const data = await myFetch("user/v1/me", {
+    method: "GET",
   })
 
   return data
