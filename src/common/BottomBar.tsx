95810b461083b8cf13eaec4de4cdc814bf6a52d5
minsu-zip <alstn4814@gmail.com>
2024-07-13 00:22:39 +0900

    feat: 링크 태그 추가

--- src/common/BottomBar.tsx
+++ src/common/BottomBar.tsx
@@ -26,9 +26,11 @@ const Album = () => {
         <Icon name="albumBold" size={28} color="gray-800" />
         <span className="text-gray-800">내 앨범</span>
       </button>
-      <div className="flex h-[54px] w-[54px] items-center justify-center rounded-full bg-gray-100">
-        <Icon name="scannerOutline" size={28} color="gray-400" />
-      </div>
+      <Link href="/scanner">
+        <div className="flex h-[54px] w-[54px] items-center justify-center rounded-full bg-gray-100">
+          <Icon name="scannerOutline" size={28} color="gray-400" />
+        </div>
+      </Link>
       <Link href="/profile">
         <button className="flex flex-col items-center gap-[2px]">
           <Icon name="userCircleOutline" size={28} color="gray-400" />
@@ -48,13 +50,17 @@ const Scanner = () => {
           <span>내 앨범</span>
         </button>
       </Link>
-      <div className="flex h-[54px] w-[54px] items-center justify-center rounded-full bg-white">
-        <Icon name="scannerOutline" size={28} color="green-600" />
-      </div>
-      <button className="flex flex-col items-center gap-[2px]">
-        <Icon name="userCircleOutline" size={28} color="gray-600" />
-        <span>마이</span>
-      </button>
+      <Link href="/scanner">
+        <div className="flex h-[54px] w-[54px] items-center justify-center rounded-full bg-white">
+          <Icon name="scannerOutline" size={28} color="green-600" />
+        </div>
+      </Link>
+      <Link href="/profile">
+        <button className="flex flex-col items-center gap-[2px]">
+          <Icon name="userCircleOutline" size={28} color="gray-600" />
+          <span>마이</span>
+        </button>
+      </Link>
     </>
   )
 }
@@ -68,9 +74,11 @@ const Profile = () => {
           <span>내 앨범</span>
         </button>
       </Link>
-      <div className="flex h-[54px] w-[54px] items-center justify-center rounded-full bg-white">
-        <Icon name="scannerOutline" size={28} color="green-600" />
-      </div>
+      <Link href="/scanner">
+        <div className="flex h-[54px] w-[54px] items-center justify-center rounded-full bg-white">
+          <Icon name="scannerOutline" size={28} color="green-600" />
+        </div>
+      </Link>
       <Link href="/profile">
         <button className="flex flex-col items-center gap-[2px]">
           <Icon name="userCircleBold" size={28} color="gray-800" />
