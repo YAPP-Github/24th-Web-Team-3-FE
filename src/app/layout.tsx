6f53963fe9070572ba95d6a241f7d6cc3997ae25
MayOwall <pixartive@gmail.com>
2024-07-13 16:53:17 +0900

    fix: 폰트를 pretendard variable에서 pretendard로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,53 @@ import ErrorHandlingWrapper from "@/common/ErrorHandlingWrapper"
 import { QueryProviders } from "@/common/QueryProviders"
 
 const pretendard = localFont({
-  src: "../font/PretendardVariable.woff2",
-  fallback: ["system-ui", "arial"],
-  variable: "--font-pretendard",
+  src: [
+    {
+      path: "../font/Pretendard-Thin.otf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../font/Pretendard-ExtraLight.otf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "../font/Pretendard-Light.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../font/Pretendard-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../font/Pretendard-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../font/Pretendard-SemiBold.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../font/Pretendard-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../font/Pretendard-ExtraBold.otf",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../font/Pretendard-Black.otf",
+      weight: "900",
+      style: "normal",
+    },
+  ],
 })
 
 export const metadata: Metadata = {
