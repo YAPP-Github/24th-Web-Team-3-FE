99cf3b55631b33d10018032f5d9c704261da6c0e
minsu-zip <alstn4814@gmail.com>
2024-07-19 11:48:00 +0900

    feat: react-query를 이용한 api 호출 및 에러 핸들링

--- src/app/album/[id]/_component/Header.tsx
+++ src/app/album/[id]/_component/Header.tsx
@@ -1,20 +1,19 @@
 "use client"
 
 import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
-import { getAlbum } from "@/app/api/photo"
-import { deleteAlbum } from "@/app/api/photo"
 import Icon from "@/common/Icon"
 import { ICON_COLOR_STYLE, ICON_NAME } from "@/constants"
 import { albumDetailHeaderVariants as headerVariants } from "@/styles/variants"
 import { cn } from "@/utils"
 
-import { AlbumInfo } from "../../types"
+import { useDeleteAlbum, useGetAlbum } from "../../create/hooks/useAlbum"
 import { Dialog } from "./Dialog"
 
 export const Header = ({ albumId }: { albumId: string }) => {
-  const [album, setAlbum] = useState<AlbumInfo | null>(null)
+  const { albumInfo } = useGetAlbum(albumId)
+  const { deleteAlbum } = useDeleteAlbum()
   const [isModalShown, setIsModalShown] = useState(false)
   const router = useRouter()
 
@@ -26,44 +25,32 @@ export const Header = ({ albumId }: { albumId: string }) => {
   }
 
   const handleDeleteAlbum = async () => {
-    await deleteAlbum(album!.albumId)
+    deleteAlbum({ albumId: albumInfo.albumId })
     router.push("/album")
   }
 
   const dialogProps = {
-    title: `'${album?.name}' 앨범을 삭제할까요?`,
+    title: `'${albumInfo.name}' 앨범을 삭제할까요?`,
     desc: "모든 사진도 함께 삭제되며, 복구할 수 없어요",
     confirmBtnContext: "앨범 삭제",
     onClose: onDialogCloseClick,
     onConfirm: handleDeleteAlbum,
   }
 
-  useEffect(() => {
-    const initAlbum = async (albumId: string) => {
-      const data = await getAlbum(albumId)
-      if (data) {
-        setAlbum(() => data)
-      }
-    }
-    initAlbum(albumId)
-  }, [albumId])
-
-  if (!album) {
+  if (!albumInfo) {
     return <header className={cn(headerVariants())}></header>
   }
 
+  const { type, name } = albumInfo
+
   return (
-    <header className={cn(headerVariants({ type: album.type }))}>
+    <header className={cn(headerVariants({ type }))}>
       <button onClick={() => router.push("/album")}>
         <Icon name="altArrowLeftOutline" size={28} />
       </button>
       <div className="flex gap-1 text-title-2 font-bold text-gray-800">
-        <Icon
-          name={ICON_NAME[album.type]}
-          color={ICON_COLOR_STYLE[album.type]}
-          size={28}
-        />
-        {album.name}
+        <Icon name={ICON_NAME[type]} color={ICON_COLOR_STYLE[type]} size={28} />
+        {name}
       </div>
       <button
         className="text-body-1 font-medium text-red-600"
--- src/app/album/create/hooks/useAlbum.ts
+++ src/app/album/create/hooks/useAlbum.ts
@@ -1,6 +1,6 @@
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useSuspenseQuery } from "@tanstack/react-query"
 
-import { postAlbum } from "@/app/api/photo"
+import { deleteAlbum, getAlbum, getAlbums, postAlbum } from "@/app/api/photo"
 import { useAlert } from "@/store/AlertContext"
 
 import { AlbumType } from "../../types"
@@ -19,4 +19,39 @@ export const usePostAlbum = () => {
   })
 
   return { albumInfo: data, postAlbum: mutate, isPending }
+}
+
+export const useGetAlbums = () => {
+  const { data } = useSuspenseQuery({
+    queryKey: ["getAlbums"],
+    queryFn: getAlbums,
+  })
+
+  return {
+    albums: data,
+  }
+}
+
+export const useGetAlbum = (albumId: string) => {
+  const { data } = useSuspenseQuery({
+    queryKey: ["getAlbum", albumId],
+    queryFn: () => getAlbum(albumId),
+  })
+
+  return { albumInfo: data }
+}
+
+export const useDeleteAlbum = () => {
+  const { showAlert } = useAlert()
+
+  const { mutate } = useMutation({
+    mutationFn: ({ albumId }: { albumId: string }) => deleteAlbum(albumId),
+    mutationKey: ["deleteAlbum"],
+    onError: (error) => {
+      showAlert("앗! 앨범을 삭제하지 못했어요", error.message)
+    },
+    throwOnError: true,
+  })
+
+  return { deleteAlbum: mutate }
 }
