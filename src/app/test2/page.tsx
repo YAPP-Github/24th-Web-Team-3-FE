a861715823245389fdf80668c41f10159985d68f
minsu-zip <alstn4814@gmail.com>
2024-07-23 20:29:17 +0900

    chore

--- src/app/test2/page.tsx
+++ src/app/test2/page.tsx
@@ -1,68 +1,31 @@
 "use client"
 
-import React, { useCallback, useRef, useState } from "react"
-import Webcam from "react-webcam"
-
-const videoConstraints = {
-  width: 1280,
-  height: 720,
-  //   facingMode: "user",
-  facingMode: { exact: "environment" },
-}
+import { IDetectedBarcode, Scanner } from "@yudiel/react-qr-scanner"
+import React from "react"
 
 const Test2Page = () => {
-  const [isCaptureEnable, setCaptureEnable] = useState<boolean>(false)
-  const webcamRef = useRef<Webcam>(null)
-  const [url, setUrl] = useState<string | null>(null)
-  const capture = useCallback(() => {
-    const imageSrc = webcamRef.current?.getScreenshot()
-    if (imageSrc) {
-      setUrl(imageSrc)
-    }
-  }, [webcamRef])
+  const [value, setValue] = React.useState<string>("")
+  const onScan = (result: IDetectedBarcode[]) => {
+    const { rawValue } = result[0]
+
+    setValue(rawValue)
+  }
 
   return (
-    <div className="h-[100vh] w-full">
-      <header>
-        <h1>camera app</h1>
-      </header>
-      {isCaptureEnable || (
-        <button onClick={() => setCaptureEnable(true)}>start</button>
-      )}
-      {isCaptureEnable && (
-        <>
-          <div>
-            <button onClick={() => setCaptureEnable(false)}>end </button>
-          </div>
-          <div>
-            <Webcam
-              audio={false}
-              width={"100%"}
-              height={"100%"}
-              ref={webcamRef}
-              screenshotFormat="image/jpeg"
-              videoConstraints={videoConstraints}
-            />
-          </div>
-          <button onClick={capture}>capture</button>
-        </>
-      )}
-      {url && (
-        <>
-          <div>
-            <button
-              onClick={() => {
-                setUrl(null)
-              }}>
-              delete
-            </button>
-          </div>
-          <div>
-            <img src={url} alt="Screenshot" />
-          </div>
-        </>
-      )}
-    </div>
+    <>
+      <Scanner
+        onScan={onScan}
+        allowMultiple={true}
+        components={{
+          audio: true,
+          onOff: true,
+          torch: true,
+          zoom: true,
+          finder: true,
+        }}></Scanner>
+
+      <p>{value}</p>
+    </>
   )
 }
 
