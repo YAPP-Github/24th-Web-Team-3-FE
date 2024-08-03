44b8ab32000a03fee3b5d1cd1f60e63eed67ba0d
minsu-zip <alstn4814@gmail.com>
2024-08-03 15:53:49 +0900

    feat: auth 경로 변경 및 로그아웃 플로우 변경

--- src/app/api/signout.ts
+++
@@ -1,9 +0,0 @@
-import { cookies } from "next/headers"
-
-import { ACCESS_TOKEN_KEY } from "@/constants"
-
-export async function GET() {
-  cookies().delete(ACCESS_TOKEN_KEY)
-
-  return new Response("/", { status: 200 })
-}
