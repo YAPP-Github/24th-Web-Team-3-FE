87286a654f8696748f24099d13c8a3f9d0f97498
minsu-zip <alstn4814@gmail.com>
2024-08-04 01:30:30 +0900

    feat: token 복호화

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from "next/server"
 import { getToken } from "next-auth/jwt"
 
+const secret = process.env.NEXT_PUBLIC_NEXTAUTH_SECRET
+
 export default async function middleware(req: NextRequest) {
-  const token = await getToken({ req })
+  const token = await getToken({ req, secret })
 
   if (!token) {
     return NextResponse.redirect(process.env.NEXT_PUBLIC_URL as string)
