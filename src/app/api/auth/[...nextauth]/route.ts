96c94674be2823d8f192c3703951eac7b0131c1c
minsu-zip <alstn4814@gmail.com>
2024-08-04 00:04:09 +0900

    refactor: next-auth 버전 다운그레이드
    auth v5에서 amplify 환경변수 인식 문제로 인한 테스트

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,60 @@
-export { GET, POST } from "@/auth"
+import { cookies } from "next/headers"
+import NextAuth from "next-auth"
+import KakaoProvider from "next-auth/providers/kakao"
+
+import { authLogin } from "@/app/api/signIn"
+import { ACCESS_TOKEN_KEY } from "@/constants"
+
+const handler = NextAuth({
+  pages: {
+    signIn: "/",
+  },
+  providers: [
+    KakaoProvider({
+      clientId: process.env.AUTH_KAKAO_ID as string,
+      clientSecret: process.env.AUTH_KAKAO_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async signIn({ account }) {
+      if (account?.access_token) {
+        const authResponse = await authLogin(account.access_token)
+
+        cookies().set(ACCESS_TOKEN_KEY, authResponse.accessToken)
+
+        const updatedAccount = {
+          ...account,
+          access_token: authResponse.accessToken,
+          refresh_token: authResponse.refreshToken,
+        }
+        account = updatedAccount
+      }
+
+      return true
+    },
+    jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+        token.refreshToken = account.refresh_token
+      }
+      return token
+    },
+    session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          ...token,
+        },
+      }
+    },
+  },
+  events: {
+    signOut() {
+      cookies().delete(ACCESS_TOKEN_KEY)
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+})
+
+export { handler as GET, handler as POST }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,10 @@
-import { NextResponse } from "next/server"
+import { NextRequest, NextResponse } from "next/server"
+import { getToken } from "next-auth/jwt"
 
-import { auth } from "./auth"
+export default async function middleware(req: NextRequest) {
+  const token = await getToken({ req })
 
-export async function middleware() {
-  const session = await auth()
-  if (!session) {
+  if (!token) {
     return NextResponse.redirect(process.env.NEXT_PUBLIC_URL as string)
   }
 }
