5ad5f842e5a5a03ff651f392b430d2f1a096a003
MayOwall <pixartive@gmail.com>
2024-08-06 12:58:06 +0900

    feat: 새앨범생성버튼, 앨범아이템 active 스타일 정의

--- src/common/FloatingButton.tsx
+++
@@ -1,9 +0,0 @@
-import SquareButton, {
-  ButtonProps as FloatingButtonProps,
-} from "./SquareButton"
-
-const FloatingButton = ({ className, ...props }: FloatingButtonProps) => {
-  return <SquareButton className={`${className} rounded-full`} {...props} />
-}
-
-export default FloatingButton
