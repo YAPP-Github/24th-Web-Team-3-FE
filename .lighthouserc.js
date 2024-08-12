8d6791c0a0babea5a21cf3b9a110b81d623c5770
minsu-zip <alstn4814@gmail.com>
2024-08-12 20:12:11 +0900

    chore: 빌드 경로 변경

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -1,7 +1,7 @@
 module.exports = {
   ci: {
     collect: {
-      staticDistDir: "./dist", // 이 경로를 실제 빌드된 정적 파일이 위치한 경로로 변경하세요.
+      staticDistDir: ".next", // 이 경로를 실제 빌드된 정적 파일이 위치한 경로로 변경하세요.
     },
     upload: {
       target: "temporary-public-storage", // CI 환경에서 결과를 임시로 저장
