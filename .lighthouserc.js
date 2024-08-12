61e04975c06ba1b0e152dab48cc3e636a19a26c7
minsu-zip <alstn4814@gmail.com>
2024-08-12 19:46:51 +0900

    chore: 경로 정의를 위한 설정 파일 추가

---
+++ .lighthouserc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: "./dist", // 이 경로를 실제 빌드된 정적 파일이 위치한 경로로 변경하세요.
+    },
+    upload: {
+      target: "temporary-public-storage", // CI 환경에서 결과를 임시로 저장
+    },
+  },
+}
