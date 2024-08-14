98f20c7ba9c1776977d7e76102f9d76ec8240531
minsu-zip <alstn4814@gmail.com>
2024-08-15 04:40:46 +0900

    chore: remove

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -3,7 +3,6 @@ module.exports = {
     collect: {
       startServerCommand: "pnpm run dev", // 서버 실행
       startServerReadyPattern: "ready on",
-      startServerReadyTimeout: 20000, // 20초로 설정
       url: ["http://localhost:3000"], // 실행할 주소
       numberOfRuns: 3, // 실행 횟수
       settings: {
@@ -15,15 +14,6 @@ module.exports = {
       target: "filesystem",
       outputDir: "./lhci_reports",
       reportFilenamePattern: "%%PATHNAME%%-%%DATETIME%%-report.%%EXTENSION%%",
-    },
-    assert: {
-      // 점수 기준 정리
-      assertions: {
-        "categories:performance": ["warn", { minScore: 0.8 }],
-        "categories:accessibility": ["warn", { minScore: 0.8 }],
-        "categories:best-practices": ["warn", { minScore: 0.8 }],
-        "categories:seo": ["warn", { minScore: 0.8 }],
-      },
     },
   },
 }
