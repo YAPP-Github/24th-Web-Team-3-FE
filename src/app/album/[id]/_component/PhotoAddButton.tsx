3e301a60340d349bdf923d7cc78906d74e3a93bd
MayOwall <pixartive@gmail.com>
2024-08-15 09:43:22 +0900

    fix: 앨범 정렬이 한쪽으로 몰리던 문제 해결

--- src/app/album/[id]/_component/PhotoAddButton.tsx
+++ src/app/album/[id]/_component/PhotoAddButton.tsx
@@ -18,8 +18,8 @@ export const PhotoAddButton = ({ albumId }: PhotoAddButtonProps) => {
   return (
     <Button
       onClick={onClick}
-      className="h-[165px] w-[165px] overflow-hidden rounded-xl">
-      <div className="mb-[13px] flex h-[165px] w-[165px] items-center justify-center rounded-xl border border-gray-200 bg-gray-100">
+      className="aspect-square w-full overflow-hidden rounded-xl">
+      <div className="mb-[13px] flex aspect-square w-full items-center justify-center rounded-xl border border-gray-200 bg-gray-100">
         <Icon name="galleryAddOutline" size={56} color="gray-300" />
       </div>
     </Button>
