ac87b0e3dc327336cc1041ad67d44e795b8985ed
minsu-zip <alstn4814@gmail.com>
2024-08-21 01:03:32 +0900

    test

--- src/auth.ts
+++ src/auth.ts
@@ -10,110 +10,109 @@ import { ACCESS_TOKEN_KEY } from "@/constants"
 
 import { useAuthStore } from "./store/auth"
 
+const getAppleToken = async () => {
+  //토큰 생성시간
+  const issuedAt = new Date().getTime() / 1000
+
+  //expireTime이 만료되면 해당 apple login이 되지 않는다.
+  //토큰생성은 배포시점에 next sever가 실행되는 처음 한번만 생성된다.
+  //6개월, 이를 초과할 경우도 login이 되지않는다.(원인은 애플정책일것으로 추측)
+  const expireTime = issuedAt + 60 * 60 * 24 * 30 * 6
+
+  const appleToken = await new SignJWT({})
+    .setAudience("https://appleid.apple.com")
+    .setIssuer(process.env.AUTH_APPLE_TEAM_ID as string)
+    .setIssuedAt(issuedAt)
+    .setExpirationTime(expireTime)
+    .setSubject(process.env.AUTH_APPLE_CLIENT_ID as string)
+    .setProtectedHeader({
+      alg: "ES256",
+      kid: process.env.AUTH_APPLE_KEY_ID,
+    })
+    .sign(createPrivateKey(process.env.AUTH_APPLE_PRIVATE_KEY as string))
+
+  return appleToken
+}
+
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
-} = NextAuth(async () => {
-  const getAppleToken = async () => {
-    //토큰 생성시간
-    const issuedAt = new Date().getTime() / 1000
+} = NextAuth({
+  /**
+   * 애플 로그인 시 쿠키옵션 적용해 주어야 callbackUrl이 정상 작동
+   */
+  cookies: {
+    callbackUrl: {
+      name: `__Secure-next-auth.callback-url`,
+      options: {
+        httpOnly: false,
+        sameSite: "none",
+        path: "/",
+        secure: true,
+      },
+    },
+  },
+  trustHost: true,
+  pages: {
+    signIn: "/",
+  },
+  providers: [
+    KakaoProvider({
+      clientId: process.env.AUTH_KAKAO_ID,
+      clientSecret: process.env.AUTH_KAKAO_SECRET,
+    }),
+    AppleProvider({
+      clientId: process.env.AUTH_APPLE_CLIENT_ID,
+      clientSecret: await getAppleToken(),
+    }),
+  ],
+  callbacks: {
+    async signIn({ account }) {
+      if (account?.access_token) {
+        const authResponse = await authLogin(account.access_token)
 
-    //expireTime이 만료되면 해당 apple login이 되지 않는다.
-    //토큰생성은 배포시점에 next sever가 실행되는 처음 한번만 생성된다.
-    //6개월, 이를 초과할 경우도 login이 되지않는다.(원인은 애플정책일것으로 추측)
-    const expireTime = issuedAt + 60 * 60 * 24 * 30 * 6
-
-    const appleToken = await new SignJWT({})
-      .setAudience("https://appleid.apple.com")
-      .setIssuer(process.env.AUTH_APPLE_TEAM_ID as string)
-      .setIssuedAt(issuedAt)
-      .setExpirationTime(expireTime)
-      .setSubject(process.env.AUTH_APPLE_CLIENT_ID as string)
-      .setProtectedHeader({
-        alg: "ES256",
-        kid: process.env.AUTH_APPLE_KEY_ID,
-      })
-      .sign(createPrivateKey(process.env.AUTH_APPLE_PRIVATE_KEY as string))
-
-    return appleToken
-  }
-  return {
-    /**
-     * 애플 로그인 시 쿠키옵션 적용해 주어야 callbackUrl이 정상 작동
-     */
-    cookies: {
-      callbackUrl: {
-        name: `__Secure-next-auth.callback-url`,
-        options: {
-          httpOnly: false,
-          sameSite: "none",
+        cookies().set(ACCESS_TOKEN_KEY, authResponse.accessToken, {
+          httpOnly: true,
+          sameSite: "lax",
           path: "/",
-          secure: true,
+          maxAge: 30 * 24 * 60 * 60, // 30일
+        })
+
+        const updatedAccount = {
+          ...account,
+          access_token: authResponse.accessToken,
+          refresh_token: authResponse.refreshToken,
+        }
+        account = updatedAccount
+      }
+
+      return true
+    },
+    jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+        token.refreshToken = account.refresh_token
+      }
+      return token
+    },
+    session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          ...token,
         },
-      },
+      }
     },
-    trustHost: true,
-    pages: {
-      signIn: "/",
+  },
+  events: {
+    signOut() {
+      cookies().delete(ACCESS_TOKEN_KEY)
+      useAuthStore.getState().clearAuth()
+      return
     },
-    providers: [
-      KakaoProvider({
-        clientId: process.env.AUTH_KAKAO_ID,
-        clientSecret: process.env.AUTH_KAKAO_SECRET,
-      }),
-      AppleProvider({
-        clientId: process.env.AUTH_APPLE_CLIENT_ID,
-        clientSecret: await getAppleToken(),
-      }),
-    ],
-    callbacks: {
-      async signIn({ account }) {
-        if (account?.access_token) {
-          const authResponse = await authLogin(account.access_token)
-
-          cookies().set(ACCESS_TOKEN_KEY, authResponse.accessToken, {
-            httpOnly: true,
-            sameSite: "lax",
-            path: "/",
-            maxAge: 30 * 24 * 60 * 60, // 30일
-          })
-
-          const updatedAccount = {
-            ...account,
-            access_token: authResponse.accessToken,
-            refresh_token: authResponse.refreshToken,
-          }
-          account = updatedAccount
-        }
-
-        return true
-      },
-      jwt({ token, account }) {
-        if (account) {
-          token.accessToken = account.access_token
-          token.refreshToken = account.refresh_token
-        }
-        return token
-      },
-      session({ session, token }) {
-        return {
-          ...session,
-          user: {
-            ...session.user,
-            ...token,
-          },
-        }
-      },
-    },
-    events: {
-      signOut() {
-        cookies().delete(ACCESS_TOKEN_KEY)
-        useAuthStore.getState().clearAuth()
-        return
-      },
-    },
-    secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
-  }
+  },
+  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
 })
