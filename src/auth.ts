77450dfaa38e80dd1b24490c109dd35fca8e2146
minsu-zip <alstn4814@gmail.com>
2024-08-22 21:27:35 +0900

    test

--- src/auth.ts
+++ src/auth.ts
@@ -39,82 +39,81 @@ export const {
   auth,
   signIn,
   signOut,
-} = NextAuth(async () => {
+} = NextAuth({
   /**
    * 애플 로그인 시 쿠키옵션 적용해 주어야 callbackUrl이 정상 작동
    */
-  return {
-    cookies: {
-      callbackUrl: {
-        name: `__Secure-next-auth.callback-url`,
-        options: {
-          httpOnly: false,
-          sameSite: "none",
+
+  cookies: {
+    callbackUrl: {
+      name: `__Secure-next-auth.callback-url`,
+      options: {
+        httpOnly: false,
+        sameSite: "none",
+        path: "/",
+        secure: true,
+      },
+    },
+  },
+  trustHost: true,
+  pages: {
+    signIn: "/",
+  },
+  providers: [
+    KakaoProvider({
+      clientId: process.env.AUTH_KAKAO_ID,
+      clientSecret: process.env.AUTH_KAKAO_SECRET,
+    }),
+    AppleProvider({
+      clientId: process.env.AUTH_APPLE_CLIENT_ID,
+      clientSecret: await getAppleToken(),
+    }),
+  ],
+  callbacks: {
+    async signIn({ account }) {
+      if (account?.access_token) {
+        const authResponse = await authLogin(account.access_token)
+
+        cookies().set(ACCESS_TOKEN_KEY, authResponse.accessToken, {
+          httpOnly: true,
+          sameSite: "lax",
           path: "/",
-          secure: true,
+          maxAge: 30 * 24 * 60 * 60, // 30일
+        })
+
+        const updatedAccount = {
+          ...account,
+          access_token: authResponse.accessToken,
+          refresh_token: authResponse.refreshToken,
+        }
+        account = updatedAccount
+      }
+
+      return true
+    },
+    jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token
+        token.refreshToken = account.refresh_token
+      }
+      return token
+    },
+    session({ session, token }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          ...token,
         },
-      },
+      }
     },
-    trustHost: true,
-    pages: {
-      signIn: "/",
+  },
+  events: {
+    signOut() {
+      cookies().delete(ACCESS_TOKEN_KEY)
+      useAuthStore.getState().clearAuth()
+      return
     },
-    providers: [
-      KakaoProvider({
-        clientId: process.env.AUTH_KAKAO_ID,
-        clientSecret: process.env.AUTH_KAKAO_SECRET,
-      }),
-      AppleProvider({
-        clientId: process.env.AUTH_APPLE_CLIENT_ID,
-        clientSecret: await getAppleToken(),
-      }),
-    ],
-    callbacks: {
-      async signIn({ account }) {
-        if (account?.access_token) {
-          const authResponse = await authLogin(account.access_token)
-
-          cookies().set(ACCESS_TOKEN_KEY, authResponse.accessToken, {
-            httpOnly: true,
-            sameSite: "lax",
-            path: "/",
-            maxAge: 30 * 24 * 60 * 60, // 30일
-          })
-
-          const updatedAccount = {
-            ...account,
-            access_token: authResponse.accessToken,
-            refresh_token: authResponse.refreshToken,
-          }
-          account = updatedAccount
-        }
-
-        return true
-      },
-      jwt({ token, account }) {
-        if (account) {
-          token.accessToken = account.access_token
-          token.refreshToken = account.refresh_token
-        }
-        return token
-      },
-      session({ session, token }) {
-        return {
-          ...session,
-          user: {
-            ...session.user,
-            ...token,
-          },
-        }
-      },
-    },
-    events: {
-      signOut() {
-        cookies().delete(ACCESS_TOKEN_KEY)
-        useAuthStore.getState().clearAuth()
-        return
-      },
-    },
-    secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
-  }
+  },
+  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
 })
