e2d014720bb052b98a6252a7bf9ecc55aeeee374
위영민 <youngminieo1005@gmail.com>
2024-08-25 03:15:50 +0900

    test

--- src/app/api/myfetch/index.ts
+++ src/app/api/myfetch/index.ts
@@ -9,7 +9,6 @@ export const myFetch = customFetch({
   },
   credentials: "include",
   interceptors: {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     request: async ([url, options], fetch) => {
       // 요청 인터셉터 로직 추가 (예: 로깅, 인증 토큰 추가)
       //   console.log("Request Interceptor:", url, options)
@@ -23,7 +22,6 @@ export const myFetch = customFetch({
       }
       return [url, options]
     },
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     response: async (response, [url, options], fetch) => {
       // 응답 인터셉터 로직 추가 (예: 에러 처리, 응답 데이터 변환)
       //   console.log("Response Interceptor:", response, url, options, fetch)
