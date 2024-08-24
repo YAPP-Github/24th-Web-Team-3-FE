af060211bb49c103c4c98a0130d15cecd0e5745d
위영민 <youngminieo1005@gmail.com>
2024-08-25 03:39:34 +0900

    test

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,5 @@
-// import { createPrivateKey } from "crypto"
-// import { SignJWT } from "jose"
+import { createPrivateKey } from "crypto"
+import { SignJWT } from "jose"
 import { cookies } from "next/headers"
 import NextAuth from "next-auth"
 import AppleProvider from "next-auth/providers/apple"
@@ -10,110 +10,116 @@ import { ACCESS_TOKEN_KEY } from "@/constants"
 
 import { useAuthStore } from "./store/auth"
 
-// const getAppleToken = async () => {
-//   //토큰 생성시간
-//   const issuedAt = new Date().getTime() / 1000
-
-//   //expireTime이 만료되면 해당 apple login이 되지 않는다.
-//   //토큰생성은 배포시점에 next sever가 실행되는 처음 한번만 생성된다.
-//   //6개월, 이를 초과할 경우도 login이 되지않는다.(원인은 애플정책일것으로 추측)
-//   const expireTime = issuedAt + 60 * 60 * 24 * 30 * 6
-
-//   const appleToken = await new SignJWT({})
-//     .setAudience("https://appleid.apple.com")
-//     .setIssuer(process.env.AUTH_APPLE_TEAM_ID as string)
-//     .setIssuedAt(issuedAt)
-//     .setExpirationTime(expireTime)
-//     .setSubject(process.env.AUTH_APPLE_CLIENT_ID as string)
-//     .setProtectedHeader({
-//       alg: "ES256",
-//       kid: process.env.AUTH_APPLE_KEY_ID,
-//     })
-//     .sign(createPrivateKey(process.env.AUTH_APPLE_PRIVATE_KEY as string))
-
-//   return appleToken
-// }
-
 export const {
   handlers: { GET, POST },
   auth,
   signIn,
   signOut,
-} = NextAuth({
-  /**
-   * 애플 로그인 시 쿠키옵션 적용해 주어야 callbackUrl이 정상 작동
-   */
+} = NextAuth(async () => {
+  const getAppleToken = async () => {
+    //토큰 생성시간
+    const issuedAt = new Date().getTime() / 1000
 
-  cookies: {
-    callbackUrl: {
-      name: `__Secure-next-auth.callback-url`,
-      options: {
-        httpOnly: false,
-        sameSite: "none",
-        path: "/",
-        secure: true,
+    //expireTime이 만료되면 해당 apple login이 되지 않는다.
+    //토큰생성은 배포시점에 next sever가 실행되는 처음 한번만 생성된다.
+    //6개월, 이를 초과할 경우도 login이 되지않는다.(원인은 애플정책일것으로 추측)
+    const expireTime = issuedAt + 60 * 60 * 24 * 30 * 6
+
+    const appleToken = await new SignJWT({})
+      .setAudience("https://appleid.apple.com")
+      .setIssuer(process.env.AUTH_APPLE_TEAM_ID as string)
+      .setIssuedAt(issuedAt)
+      .setExpirationTime(expireTime)
+      .setSubject(process.env.AUTH_APPLE_CLIENT_ID as string)
+      .setProtectedHeader({
+        alg: "ES256",
+        kid: process.env.AUTH_APPLE_KEY_ID,
+      })
+      .sign(
+        createPrivateKey(
+          `-----BEGIN PRIVATE KEY-----\n${process.env.AUTH_APPLE_PRIVATE_KEY ?? ""}\n-----END PRIVATE KEY-----\n`
+        )
+      )
+
+    return appleToken
+  }
+
+  return {
+    /**
+     * 애플 로그인 시 쿠키옵션 적용해 주어야 callbackUrl이 정상 작동
+     */
+
+    cookies: {
+      callbackUrl: {
+        name: `__Secure-next-auth.callback-url`,
+        options: {
+          httpOnly: false,
+          sameSite: "none",
+          path: "/",
+          secure: true,
+        },
       },
     },
-  },
-  trustHost: true,
-  pages: {
-    signIn: "/",
-  },
-  providers: [
-    KakaoProvider({
-      clientId: process.env.AUTH_KAKAO_ID,
-      clientSecret: process.env.AUTH_KAKAO_SECRET,
-    }),
-    AppleProvider({
-      clientId: process.env.AUTH_APPLE_CLIENT_ID,
-      clientSecret: `-----BEGIN PRIVATE KEY-----\n${process.env.AUTH_APPLE_PRIVATE_KEY ?? ""}\n-----END PRIVATE KEY-----\n`,
-    }),
-  ],
-  callbacks: {
-    async signIn({ account }) {
-      if (account?.access_token) {
-        const authResponse = await authLogin(account.access_token)
+    trustHost: true,
+    pages: {
+      signIn: "/",
+    },
+    providers: [
+      KakaoProvider({
+        clientId: process.env.AUTH_KAKAO_ID,
+        clientSecret: process.env.AUTH_KAKAO_SECRET,
+      }),
+      AppleProvider({
+        clientId: process.env.AUTH_APPLE_CLIENT_ID,
+        clientSecret: await getAppleToken(),
+      }),
+    ],
+    callbacks: {
+      async signIn({ account }) {
+        if (account?.access_token) {
+          const authResponse = await authLogin(account.access_token)
 
-        cookies().set(ACCESS_TOKEN_KEY, authResponse.accessToken, {
-          httpOnly: true,
-          sameSite: "lax",
-          path: "/",
-          maxAge: 30 * 24 * 60 * 60, // 30일
-        })
+          cookies().set(ACCESS_TOKEN_KEY, authResponse.accessToken, {
+            httpOnly: true,
+            sameSite: "lax",
+            path: "/",
+            maxAge: 30 * 24 * 60 * 60, // 30일
+          })
 
-        const updatedAccount = {
-          ...account,
-          access_token: authResponse.accessToken,
-          refresh_token: authResponse.refreshToken,
+          const updatedAccount = {
+            ...account,
+            access_token: authResponse.accessToken,
+            refresh_token: authResponse.refreshToken,
+          }
+          account = updatedAccount
         }
-        account = updatedAccount
-      }
 
-      return true
+        return true
+      },
+      jwt({ token, account }) {
+        if (account) {
+          token.accessToken = account.access_token
+          token.refreshToken = account.refresh_token
+        }
+        return token
+      },
+      session({ session, token }) {
+        return {
+          ...session,
+          user: {
+            ...session.user,
+            ...token,
+          },
+        }
+      },
     },
-    jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token
-        token.refreshToken = account.refresh_token
-      }
-      return token
+    events: {
+      signOut() {
+        cookies().delete(ACCESS_TOKEN_KEY)
+        useAuthStore.getState().clearAuth()
+        return
+      },
     },
-    session({ session, token }) {
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          ...token,
-        },
-      }
-    },
-  },
-  events: {
-    signOut() {
-      cookies().delete(ACCESS_TOKEN_KEY)
-      useAuthStore.getState().clearAuth()
-      return
-    },
-  },
-  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
+    secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
+  }
 })
