16fc56ef9c80b1d8e7ee61b0b432679280648f2d
ChuYong <jakgon@naver.com>
2024-08-28 22:06:43 +0900

    feat: 애플 로그인 버튼 추가

--- src/app/_component/LoginButton.tsx
+++ src/app/_component/LoginButton.tsx
@@ -6,17 +6,29 @@ import Icon from "@/common/Icon"
 import SquareButton from "@/common/SquareButton"
 
 const LoginButton = () => {
-  const handleSignIn = async () => {
+  const handleSignInWithKakao = async () => {
     await signIn("kakao", { callbackUrl: "/scanner" })
   }
 
+  const handleSignInWithApple = async () => {
+    await signIn("apple", { callbackUrl: "/scanner" })
+  }
+
   return (
-    <SquareButton
-      onClick={handleSignIn}
-      className="w-full bg-kakao-600 text-gray-1000 active:bg-kakao-700">
-      <Icon name={"kakaoLogo"} size={28}></Icon>
-      카카오로 3초만에 계속하기
-    </SquareButton>
+    <div className="flex flex-col gap-2">
+      <SquareButton
+        onClick={handleSignInWithKakao}
+        className="w-full bg-kakao-600 text-gray-1000 active:bg-kakao-700">
+        <Icon name={"kakaoLogo"} size={28}></Icon>
+        카카오로 3초만에 계속하기
+      </SquareButton>
+      <SquareButton
+        onClick={handleSignInWithApple}
+        className="w-full bg-gray-950 text-gray-50 active:bg-gray-1000">
+        <Icon name={"appleLogo"} size={24} color={"gray-50"}></Icon>
+        Apple로 로그인하기
+      </SquareButton>
+    </div>
   )
 }
 
