15c4e09b733d185b2766eba8d47588cde30ebcb3
ChuYong <jakgon@naver.com>
2024-08-30 00:42:57 +0900

    feat: 애플로그인 수리

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,1 +1,9 @@
-export { GET, POST } from "@/auth"
+import type { NextApiRequest, NextApiResponse } from "next"
+
+import authMapper from "@/auth"
+
+async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await authMapper(req, res)
+}
+
+export { auth as GET, auth as POST }
--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,9 @@
+import { createPrivateKey } from "crypto"
+import { SignJWT } from "jose"
+import type { NextApiRequest, NextApiResponse } from "next"
 import { cookies } from "next/headers"
 import NextAuth from "next-auth"
+import AppleProvider from "next-auth/providers/apple"
 import KakaoProvider from "next-auth/providers/kakao"
 
 import { authLogin } from "@/app/api/signIn"
@@ -7,67 +11,91 @@ import { ACCESS_TOKEN_KEY } from "@/constants"
 
 import { useAuthStore } from "./store/auth"
 
-export const {
-  handlers: { GET, POST },
-  auth,
-  signIn,
-  signOut,
-} = NextAuth({
-  trustHost: true,
-  pages: {
-    signIn: "/",
-  },
-  providers: [
-    KakaoProvider({
-      clientId: process.env.AUTH_KAKAO_ID,
-      clientSecret: process.env.AUTH_KAKAO_SECRET,
-    }),
-  ],
-  callbacks: {
-    async signIn({ account }) {
-      if (account?.access_token) {
-        const authResponse = await authLogin(account.access_token)
+// export const {
+//   handlers: { GET, POST },
+//   auth,
+//   signIn,
+//   signOut,
+// } = )
 
-        cookies().set(ACCESS_TOKEN_KEY, authResponse.accessToken, {
-          httpOnly: true,
-          sameSite: "lax",
-          path: "/",
-          maxAge: 30 * 24 * 60 * 60, // 30일
-        })
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, {
+    pages: {
+      signIn: "/",
+    },
+    providers: [
+      KakaoProvider({
+        clientId: process.env.AUTH_KAKAO_ID ?? "",
+        clientSecret: process.env.AUTH_KAKAO_SECRET ?? "",
+      }),
+      AppleProvider({
+        clientId: process.env.APPLE_ID ?? "",
+        clientSecret: await getAppleToken(),
+      }),
+    ],
+    callbacks: {
+      async signIn({ account }) {
+        if (account?.access_token) {
+          const authResponse = await authLogin(account.access_token)
 
-        const updatedAccount = {
-          ...account,
-          access_token: authResponse.accessToken,
-          refresh_token: authResponse.refreshToken,
+          cookies().set(ACCESS_TOKEN_KEY, authResponse.accessToken, {
+            httpOnly: true,
+            sameSite: "lax",
+            path: "/",
+            maxAge: 30 * 24 * 60 * 60, // 30일
+          })
+
+          const updatedAccount = {
+            ...account,
+            access_token: authResponse.accessToken,
+            refresh_token: authResponse.refreshToken,
+          }
+          account = updatedAccount
         }
-        account = updatedAccount
-      }
 
-      return true
+        return true
+      },
+      jwt({ token, account }) {
+        if (account) {
+          token.accessToken = account.access_token
+          token.refreshToken = account.refresh_token
+        }
+        return token
+      },
+      session({ session, token }) {
+        return {
+          ...session,
+          user: {
+            ...session.user,
+            ...token,
+          },
+        }
+      },
     },
-    jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token
-        token.refreshToken = account.refresh_token
-      }
-      return token
+    events: {
+      signOut() {
+        cookies().delete(ACCESS_TOKEN_KEY)
+        useAuthStore.getState().clearAuth()
+        return
+      },
     },
-    session({ session, token }) {
-      return {
-        ...session,
-        user: {
-          ...session.user,
-          ...token,
-        },
-      }
-    },
-  },
-  events: {
-    signOut() {
-      cookies().delete(ACCESS_TOKEN_KEY)
-      useAuthStore.getState().clearAuth()
-      return
-    },
-  },
-  secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
-})
+    secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
+  })
+}
+
+const getAppleToken = async () => {
+  const key = `-----BEGIN PRIVATE KEY-----\n${process.env.APPLE_PRIVATE_KEY}\n-----END PRIVATE KEY-----\n`
+
+  const appleToken = await new SignJWT({})
+    .setAudience("https://appleid.apple.com")
+    .setIssuer(process.env.APPLE_TEAM_ID ?? "")
+    .setIssuedAt(new Date().getTime() / 1000)
+    .setExpirationTime(new Date().getTime() / 1000 + 3600 * 2)
+    .setSubject(process.env.APPLE_ID ?? "")
+    .setProtectedHeader({
+      alg: "ES256",
+      kid: process.env.APPLE_KEY_ID,
+    })
+    .sign(createPrivateKey(key))
+  return appleToken
+}
