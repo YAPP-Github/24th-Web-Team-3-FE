3524ed003e243038b27155c31e8a1789e1cad2b1
ChuYong <jakgon@naver.com>
2024-09-25 20:21:48 +0900

    feat: 리캡을 일단 롤백해요

--- src/app/album/[id]/_component/AlbumPhotos.tsx
+++ src/app/album/[id]/_component/AlbumPhotos.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg"
 import { toBlob } from "html-to-image"
 import Image from "next/image"
 import { useEffect, useRef, useState } from "react"
@@ -8,22 +7,15 @@ import Masonry from "react-responsive-masonry"
 
 import { deletePhoto, getPhotos } from "@/app/api/photo"
 import { useGetProfile } from "@/app/profile/hooks/useProfile"
-import Button from "@/common/Button"
 import Icon from "@/common/Icon"
 import { ICON_COLOR_STYLE, ICON_NAME } from "@/constants"
 import { formattedDate } from "@/libs"
-import { recapColorVariants } from "@/styles/variants"
-import { cn } from "@/utils"
 
 import { AlbumInfo, PhotoInfo } from "../../types"
 import { ImageDetail } from "./ImageDetail"
 import { Photo } from "./Photo"
 import { PhotoAddButton } from "./PhotoAddButton"
 import RecapContainer from "./RecapContainer"
-import VideoLoading from "./VideoLoading"
-import VideoRecap from "./VideoRecap"
-
-const ffmpeg = createFFmpeg({ log: true })
 
 interface AlbumPhotosProps {
   albumInfo: AlbumInfo
@@ -37,10 +29,10 @@ export const AlbumPhotos = ({ albumInfo }: AlbumPhotosProps) => {
 
   const carouselStartIdx = useRef(0)
 
-  const [isRecapOpen, setIsRecapOpen] = useState(false)
+  //const [isRecapOpen, setIsRecapOpen] = useState(false)
   const refs = useRef<(HTMLDivElement | null)[]>([])
   const [files, setFiles] = useState<File[]>([])
-  const [videoUrl, setVideoUrl] = useState<string | null>()
+  //const [videoUrl, setVideoUrl] = useState<string | null>()
 
   const onPhotoClick = (startIdx: number) => {
     carouselStartIdx.current = startIdx
@@ -62,9 +54,9 @@ export const AlbumPhotos = ({ albumInfo }: AlbumPhotosProps) => {
     }
   }
 
-  const handleCloseRecap = () => {
+  /*  const handleCloseRecap = () => {
     setIsRecapOpen(false)
-  }
+  }*/
 
   useEffect(() => {
     const fetchAlbums = async () => {
@@ -109,74 +101,74 @@ export const AlbumPhotos = ({ albumInfo }: AlbumPhotosProps) => {
     }
   }, [refs, photos.length])
 
-  useEffect(() => {
-    if (!files.length) return
-
-    const createVideo = async () => {
-      if (!ffmpeg.isLoaded()) {
-        await ffmpeg.load()
-      }
-
-      // 이미지 파일들을 FFmpeg 가상 파일 시스템에 작성
-      for (let index = 0; index < files.length; index++) {
-        const file = files[index]
-        ffmpeg.FS("writeFile", `image${index}.png`, await fetchFile(file))
-      }
-
-      try {
-        for (let index = 0; index < files.length; index++) {
-          await ffmpeg.run(
-            "-loop",
-            "1",
-            "-i",
-            `image${index}.png`,
-            "-t",
-            "1", // 각 이미지를 1초 동안 표시
-            "-vf",
-            `scale=1170:2040:force_original_aspect_ratio=increase,crop=1170:2040,pad=1170:2040:(ow-iw)/2:(oh-ih)/2,format=yuv420p`, //3배수로 해상도 조정
-            "-c:v",
-            "libx264", // H.264 코덱 사용
-            "-crf",
-            "18", // 낮은 값을 사용할수록 품질이 높고 파일 크기가 커짐
-            "-preset",
-            "slow", // 고품질 인코딩을 위한 slow 프리셋
-            "-y",
-            `video${index}.mp4`
-          )
-        }
-
-        // 생성된 비디오 파일들을 concat 필터로 연결
-        const inputArgs = files
-          .map((_, index) => `-i video${index}.mp4`)
-          .join(" ")
-        const concatFilter = `concat=n=${files.length}:v=1:a=0[outv]`
-
-        const args = [
-          ...inputArgs.split(" "),
-          "-filter_complex",
-          concatFilter,
-          "-map",
-          "[outv]",
-          "-y",
-          "output.mp4",
-        ]
-
-        await ffmpeg.run(...args)
-
-        // 파일 시스템에서 output.mp4 읽기
-        const data = ffmpeg.FS("readFile", "output.mp4")
-        const video = URL.createObjectURL(
-          new Blob([data.buffer], { type: "video/mp4" })
-        )
-        setVideoUrl(video)
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error("FFmpeg error:", error)
-      }
-    }
-
-    createVideo()
-  }, [files])
+  // useEffect(() => {
+  //   if (!files.length) return
+  //
+  //   const createVideo = async () => {
+  //     if (!ffmpeg.isLoaded()) {
+  //       await ffmpeg.load()
+  //     }
+  //
+  //     // 이미지 파일들을 FFmpeg 가상 파일 시스템에 작성
+  //     for (let index = 0; index < files.length; index++) {
+  //       const file = files[index]
+  //       ffmpeg.FS("writeFile", `image${index}.png`, await fetchFile(file))
+  //     }
+  //
+  //     try {
+  //       for (let index = 0; index < files.length; index++) {
+  //         await ffmpeg.run(
+  //           "-loop",
+  //           "1",
+  //           "-i",
+  //           `image${index}.png`,
+  //           "-t",
+  //           "1", // 각 이미지를 1초 동안 표시
+  //           "-vf",
+  //           `scale=1170:2040:force_original_aspect_ratio=increase,crop=1170:2040,pad=1170:2040:(ow-iw)/2:(oh-ih)/2,format=yuv420p`, //3배수로 해상도 조정
+  //           "-c:v",
+  //           "libx264", // H.264 코덱 사용
+  //           "-crf",
+  //           "18", // 낮은 값을 사용할수록 품질이 높고 파일 크기가 커짐
+  //           "-preset",
+  //           "slow", // 고품질 인코딩을 위한 slow 프리셋
+  //           "-y",
+  //           `video${index}.mp4`
+  //         )
+  //       }
+  //
+  //       // 생성된 비디오 파일들을 concat 필터로 연결
+  //       const inputArgs = files
+  //         .map((_, index) => `-i video${index}.mp4`)
+  //         .join(" ")
+  //       const concatFilter = `concat=n=${files.length}:v=1:a=0[outv]`
+  //
+  //       const args = [
+  //         ...inputArgs.split(" "),
+  //         "-filter_complex",
+  //         concatFilter,
+  //         "-map",
+  //         "[outv]",
+  //         "-y",
+  //         "output.mp4",
+  //       ]
+  //
+  //       await ffmpeg.run(...args)
+  //
+  //       // 파일 시스템에서 output.mp4 읽기
+  //       const data = ffmpeg.FS("readFile", "output.mp4")
+  //       const video = URL.createObjectURL(
+  //         new Blob([data.buffer], { type: "video/mp4" })
+  //       )
+  //       //setVideoUrl(video)
+  //     } catch (error) {
+  //       // eslint-disable-next-line no-console
+  //       console.error("FFmpeg error:", error)
+  //     }
+  //   }
+  //
+  //   createVideo()
+  // }, [files])
 
   return (
     <>
@@ -198,19 +190,19 @@ export const AlbumPhotos = ({ albumInfo }: AlbumPhotosProps) => {
           />
         )}
 
-        {photos.length >= 3 && (
-          <Button
-            className={cn(
-              recapColorVariants({ type: albumInfo.type }),
-              "m-auto rounded-[100px] p-14 px-[22px] py-[16px]"
-            )}
-            onClick={() => setIsRecapOpen(true)}>
-            <div className="flex gap-1 align-middle">
-              <p>리캡 만들기</p>
-              <Icon name="reelOutline" size={24} color="white" />
-            </div>
-          </Button>
-        )}
+        {/*{photos.length >= 3 && (*/}
+        {/*  <Button*/}
+        {/*    className={cn(*/}
+        {/*      recapColorVariants({ type: albumInfo.type }),*/}
+        {/*      "m-auto rounded-[100px] p-14 px-[22px] py-[16px]"*/}
+        {/*    )}*/}
+        {/*    onClick={() => setIsRecapOpen(true)}>*/}
+        {/*    <div className="flex gap-1 align-middle">*/}
+        {/*      <p>리캡 만들기</p>*/}
+        {/*      <Icon name="reelOutline" size={24} color="white" />*/}
+        {/*    </div>*/}
+        {/*  </Button>*/}
+        {/*)}*/}
       </div>
 
       <div className="fixed left-0 top-0 -translate-x-full">
@@ -271,12 +263,12 @@ export const AlbumPhotos = ({ albumInfo }: AlbumPhotosProps) => {
         ))}
       </div>
 
-      {isRecapOpen &&
-        (videoUrl ? (
-          <VideoRecap url={videoUrl} closeModal={handleCloseRecap} />
-        ) : (
-          <VideoLoading type={albumInfo.type} />
-        ))}
+      {/*{isRecapOpen &&*/}
+      {/*  (videoUrl ? (*/}
+      {/*    <VideoRecap url={videoUrl} closeModal={handleCloseRecap} />*/}
+      {/*  ) : (*/}
+      {/*    <VideoLoading type={albumInfo.type} />*/}
+      {/*  ))}*/}
     </>
   )
 }
