748be34f75b79fec30bc9db159984ae077d5bc95
ChuYong <jakgon@naver.com>
2024-11-07 22:44:47 +0900

    feat: 자동재생 활성화

--- src/app/album/[id]/_component/VideoRecap.tsx
+++ src/app/album/[id]/_component/VideoRecap.tsx
@@ -45,7 +45,7 @@ const VideoRecap = ({ url, closeModal }: VideoRecapProps) => {
   return (
     <div className="fixed left-0 top-0 z-10 h-dvh w-dvw justify-center overflow-auto bg-gray-900">
       <div className="m-auto flex h-dvh w-full max-w-[390px] flex-col justify-between">
-        <div>
+        <div className="flex h-full flex-col">
           <Icon
             className="absolute z-10 ml-2 mt-2"
             color="gray-300"
@@ -54,48 +54,41 @@ const VideoRecap = ({ url, closeModal }: VideoRecapProps) => {
             onClick={closeModal}
           />
           <ReactPlayer
-            className="absolute"
+            className="grow"
             url={url}
             playing={true}
+            muted={true}
             loop={true}
             controls={false}
             width="100%"
-            height="100%"
             config={{
               file: {
                 attributes: { crossOrigin: "anonymous" },
               },
             }}
           />
-          {/*<video*/}
-          {/*  src={url}*/}
-          {/*  width="390"*/}
-          {/*  height="680"*/}
-          {/*  crossOrigin="anonymous"*/}
-          {/*  autoPlay*/}
-          {/*  controls></video>*/}
-        </div>
-        <div className="mx-5 mb-5 flex gap-3">
-          <Button
-            onClick={handleDownload}
-            color="white"
-            className="flex h-14 w-full items-center justify-center gap-[6px] rounded-[100px] bg-gray-800 px-6 text-white">
-            <Icon name="downloadBold" size={28} color="white" />
-            <span className="mr-[6px]">다운로드</span>
-          </Button>
+          <div className="mx-5 mb-5 flex flex-row gap-3">
+            <Button
+              onClick={handleDownload}
+              color="white"
+              className="flex h-14 w-full items-center justify-center gap-[6px] rounded-[100px] bg-gray-800 px-6 text-white">
+              <Icon name="downloadBold" size={28} color="white" />
+              <span className="mr-[6px]">다운로드</span>
+            </Button>
 
-          {
-            <SquareButton
-              onClick={() => {}}
-              className="w-full rounded-[100px] bg-gray-800 bg-gradient-to-br from-yellow-500 via-pink-500 to-blue-500"
-              style={{
-                background:
-                  "linear-gradient(275deg, #FFD735 -13%, #FF6C5A 19.01%, #E848C8 52.09%, #4A8CF0 93.31%), #F0F2F4",
-              }}>
-              <Icon name="insta" size={28} color="white" />
-              <span className="mr-[6px]">인스타 공유</span>
-            </SquareButton>
-          }
+            {
+              <SquareButton
+                onClick={() => {}}
+                className="w-full rounded-[100px] bg-gray-800 bg-gradient-to-br from-yellow-500 via-pink-500 to-blue-500"
+                style={{
+                  background:
+                    "linear-gradient(275deg, #FFD735 -13%, #FF6C5A 19.01%, #E848C8 52.09%, #4A8CF0 93.31%), #F0F2F4",
+                }}>
+                <Icon name="insta" size={28} color="white" />
+                <span className="mr-[6px]">인스타 공유</span>
+              </SquareButton>
+            }
+          </div>
         </div>
       </div>
     </div>
